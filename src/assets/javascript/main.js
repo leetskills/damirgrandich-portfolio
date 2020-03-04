03f33f48f4a4f0402e6ff3580f092ca31ac0db5c
Damir Grandich <dgrandich@gmail.com>
2020-03-04 12:20:31 +0000

    Big commit - Actual design - Some project pages - DGD Branding

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -4,3 +4,17 @@ $(document).ready(function() {
     $('[data-tooltip="tooltip"]').tooltip()
   })
 })
+
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop()
+
+  if (scroll >= 80) {
+    $(".navbar")
+      .addClass("navbar-scrolled navbar-light")
+      .removeClass("navbar-dark")
+  } else {
+    $(".navbar")
+      .removeClass("navbar-scrolled navbar-light")
+      .addClass("navbar-dark")
+  }
+});
